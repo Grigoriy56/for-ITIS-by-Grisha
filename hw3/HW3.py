072a187f2a59f22cb89f5975392d3933005c08e3
Grisha <grisha9hs@gmail.com>
2021-03-01 01:51:37 +0300

    al

---
+++ hw3/HW3.py
@@ -0,0 +1,184 @@
+from matplotlib import pyplot as plt
+from math import sqrt
+
+def distance(x1, y1, x2, y2):
+	"""
+	Расчет расстояния между двумя точками
+	Args:
+		x1 y1 : Координаты 1-ой точки
+		x2 y2 : Координаты 2-ой точки
+	Returns:
+		float: расстояние между ними
+	"""
+	dist = sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+	return dist
+
+
+def read_data(path):
+	"""
+	Чтение файла и сохранение в словаре вида: ключ адрес, значения это кортеж (координата Х, коордианата Y, площадь)
+	Пример: {'пр-кт Фатыха Амирхана д 91Б': (5.73063, 11.8712, 2095.0), ... }
+	Args:
+		path (str): путь к файлу
+	Returns:
+		dict: ключ адрес, значение (x, y, площадь)
+	"""
+	data = {}
+	with open(path, 'r', encoding='utf-8') as file:
+		book = file.read()
+		book = book.split('\n')
+		book = book[:len(book) - 1:]
+		book = [i.split('\t') for i in book]
+		for i in range(len(book)):
+			data[book[i][0]] = float(book[i][1]), float(book[i][2]), float(book[i][3])
+	return data
+
+def reverse_dict(dict):
+	"""
+	Я не починил свою ide, чтобы одной командой разворачивать словарь, и написал для этого функцию ;)
+	"""
+	items = list(dict.items())
+	n_dict = {k: v for k, v in reversed(items)}
+	return n_dict
+
+def the_best(database):
+	"""
+	Как task1, только возвращащет весь список лучший от лучшего к худшему
+	"""
+	radius = {}
+	for house in database:
+		radius[house] = 0
+		for others in database:
+			if distance(database[house][0], database[house][1], database[others][0], database[others][1]) <= 0.5:
+				radius[house] += 1
+
+	sort_key = sorted(radius, key=radius.get)
+	sort_key = sort_key[::-1]
+
+	return sort_key
+
+def task1(database):
+	"""
+	Задача 1
+	Args:
+		database (dict): изначальный датасет словарь, ключ адрес, значение (x, y, площадь)
+		помимо database могут быть любые другие аргументы
+	Returns:
+		list: координаты дома (x, y)
+	"""
+
+	radius = {}
+	for house in database:
+		radius[house] = 0
+		for others in database:
+			if distance(database[house][0], database[house][1], database[others][0], database[others][1]) <= 0.5:
+				radius[house] += 1
+
+	sort_key = sorted(radius, key=radius.get)
+	k = sort_key[-1]
+	ret = [(database[k][0], database[k][1])]
+	return ret
+
+
+def task2(database):
+	"""
+	Задача 2
+	Args:
+		database (dict): изначальный датасет словарь, ключ адрес, значение (x, y, площадь)
+		помимо database могут быть любые другие аргументы
+	Returns:
+		list: координаты домов [(x1,y1), (x2,y2) ... (xn,yn)]
+	"""
+	b_list = the_best(database)
+	bad = []
+	good = []
+	while len(good) < 15:
+		for i in b_list:
+			if i not in bad:
+				good.append((database[i][0:2]))
+			for shop in database:
+				if distance(database[i][0], database[i][1], database[shop][0], database[shop][1]) <= 1:
+					bad.append(shop)
+	return good[:10]
+
+
+def task3(database):
+	"""
+	Задача 3
+	Args:
+		database (dict): изначальный датасет словарь, ключ адрес, значение (x, y, площадь)
+		помимо database могут быть любые другие аргументы
+	Returns:
+		list: координаты домов [(x1,y1), (x2,y2) ... (xn,yn)]
+	"""
+
+	list = []
+	b_list = []
+
+	def add_people(database):
+		for key in database:
+			database[key] = [database[key][0], database[key][1], database[key][2], database[key][2] * 0.7 / 18, 0]
+		return database
+
+	database = add_people(database)
+	for house in database:
+		for others in database:
+			if database[others] not in b_list:
+				if distance(database[house][0], database[house][1], database[others][0], database[others][1]) <= 0.5:
+					database[house][4] += database[others][3]
+					b_list.append(database[house])
+		# теперь в список list добавляю элементы так, чтобы первый элемент был кол-во жильцов, х, у
+		list.append([database[house][4], database[house][0], database[house][1]])
+	# по алгоритмам показали сортировку пузырьком, сейчас попробуем :D
+	for i in range(len(list) - 1):
+		for j in range(len(list) - 1 - i):
+			if list[j][0] > list[j + 1][0]:
+				list[j], list[j + 1] = list[j + 1], list[j]
+	coordinate = []
+	for i in range(15):
+		coordinate += [(list[i][1], list[i][2])]
+	return coordinate
+
+def plot(database, best_coords):
+	"""
+	НЕ МЕНЯТЬ КОД!
+	Отрисовка точек 2D
+	Args:
+		database (dict): изначальный датасет словарь, ключ адрес, значение (x, y, площадь)
+		best_coords (list): для задачи 1 это (x, y), для задачи 2-3 это [(x1,y1), (x2,y2) ... (xn,yn)]
+	"""
+	plt.close()
+	fig, ax = plt.subplots(figsize=(8, 8))
+	plt.plot([coord[0] for coord in database.values()],
+	         [coord[1] for coord in database.values()], '.', ms=5, color='k', alpha=0.5)
+	if isinstance(best_coords[0], tuple):
+		for x, y in best_coords:
+			circle = plt.Circle((x, y), 0.5, color='r', fill=False, zorder=2)
+			ax.add_patch(circle)
+		plt.plot([coord[0] for coord in best_coords],
+		         [coord[1] for coord in best_coords], '.', ms=15, color='r')
+	elif isinstance(best_coords[0], float):
+		x, y = best_coords
+		circle = plt.Circle((x, y), 0.5, color='r', fill=False, zorder=2)
+		ax.add_patch(circle)
+		plt.plot(*best_coords, '.', ms=15, color='r')
+	else:
+		raise ValueError("Проверь, что подаёшь список кортежей или кортеж из двух координат")
+	plt.show()
+
+def homework():
+	path = "/home/alex/buildings"
+	database = read_data(path)
+
+	best_task1 = task1(database)
+	plot(database, best_task1)
+
+	best_task2 = task2(database)
+	plot(database, best_task2)
+
+	best_task3 = task3(database)
+	plot(database, best_task3)
+
+if __name__ == '__main__':
+	homework()
+
